1ce18b679c7367448746a40f4fe7ae84508bb0b9
c-garraway <c-garraway@users.noreply.github.com>
2022-07-15 16:35:56 -0400

    SearchBar added

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Inventory } from '../features/inventory/Inventory.js';
 import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
+import { SearchTerm } from '../features/searchTerm/SearchTerm.js';
 // Import the Cart component here.
 import {Cart} from '../features/cart/Cart.js'
 // Render the Cart component below <Inventory />
@@ -9,15 +10,22 @@ export const App = (props) => {
 
   const { state, dispatch } = props;
 
+  const visibleInventory = getFilteredInventory(state.inventory, state.searchTerm);
+
   return (
     <div>
       <CurrencyFilter
         currencyFilter={state.currencyFilter}
         dispatch={dispatch}
       />
-
+      
+      <SearchTerm 
+        searchTerm={state.searchTerm}
+        dispatch={dispatch}
+      />
+    
       <Inventory
-        inventory={state.inventory}
+        inventory={visibleInventory}
         currencyFilter={state.currencyFilter}
         dispatch={dispatch}
       />
@@ -31,3 +39,7 @@ export const App = (props) => {
     </div>
   );
 };
+
+function getFilteredInventory(inventory, searchTerm) {
+    return inventory.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
