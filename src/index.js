6eec730afc7860d90b3a24ff87401ef722cd5233
c-garraway <c-garraway@users.noreply.github.com>
2022-07-15 15:40:55 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* import ReactDOM from 'react-dom'; */
+import { createRoot } from 'react-dom/client';
+import { App } from './app/App.js';
+// Import the store here.
+import { store } from './app/store.js';
+// Pass state and dispatch props to the <App /> component.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+const render = () => {
+  root.render(
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+    />,
+    
+  )
+};
+render();
+store.subscribe(render)
+// Subscribe render to the store.
